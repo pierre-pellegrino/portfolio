43d6fe760021340f161284be879a32996d966af1
Pierre <pierre.pellegrino@laposte.net>
2022-04-01 14:22:06 +0200

    homepage responsive

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,16 @@
 import '../styles/globals.scss';
 import { appWithTranslation } from 'next-i18next';
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default appWithTranslation(MyApp);
